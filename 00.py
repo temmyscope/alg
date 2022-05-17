6efd6f332d0c136deb754bc9d35f908a760ddf1d
Elisha OYAWALE <elishaoyawale@Elishas-MacBook-Pro.local>
2022-05-17 23:29:03 +0100

    Modify InsertSort Implementation

--- 00.py
+++ 00.py
@@ -1,2 +1,20 @@
+def insertionSort(data: list):
+    length: int = len(data)
+
+    for i in range(1, length):
+        currentValue = data[i]
+
+        j = i-1
+        while j >= 0 and currentValue < data[j]:
+            data[j+1] = data[j]
+            j -= 1
+
+        data[j+1] = currentValue
+        
+    return data
 
 
+print(
+    #[12, 2, 5, 13, 9, 7, 4, 1, 10, 15]
+    insertionSort([1, 4, 17, 3, 90, 79, 4, 6, 81])
+)
